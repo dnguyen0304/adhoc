2a6855f070e904d877abf749b96a0f2350421eee
ubuntu <dnguyen0304@gmail.com>
2017-11-19 10:35:35 -0500

    Added MergeZipCodes pipeline.

--- adhoc_slcsp/functions.py
+++ adhoc_slcsp/functions.py
@@ -25,3 +25,39 @@ class Pipeline(metaclass=abc.ABCMeta):
         """
 
         raise NotImplementedError
+
+
+class MergeZipCodes(Pipeline):
+
+    def __init__(self, reader):
+
+        """
+        Merge with the ZIP code lookup.
+
+        Parameters
+        ----------
+        reader : adhoc_slcsp.io.Reader
+        """
+
+        self._reader = reader
+
+    def __call__(self, df):
+        # This could be read into memory once and passed as a singleton
+        # dependency.
+        zip_codes = self._reader.read()
+        # Filter ZIP codes associated with multiple rate areas. The
+        # rates for these ZIP codes are ambiguous.
+        # This could be processed and persisted to avoid running the
+        # computation every time.
+        zip_codes = zip_codes.loc[:, ['zipcode', 'state', 'rate_area']] \
+                             .groupby('zipcode') \
+                             .filter(lambda x: len(x.drop_duplicates()) == 1) \
+                             .drop_duplicates()
+        # This raises an uncaught KeyError if the label cannot be
+        # found in both data frames.
+        merged = df.merge(zip_codes, how='left', on='zipcode')
+        return merged
+
+    def __repr__(self):
+        repr_ = '{}(reader={})'
+        return repr_.format(self.__class__.__name__, self._reader)
